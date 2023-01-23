efea0e7bc4111cee3738ab1645a5ca766b9d7917
tomek <tomek.jankowiak01@gmail.com>
2023-01-23 16:13:59 +0100

    lab08 - Esper: Podstawy

---
+++ 08 Esper - podstawy/src/main/java/Main.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.runtime.client.EPRuntime;
+import com.espertech.esper.runtime.client.EPRuntimeProvider;
+
+import com.espertech.esper.common.client.EPCompiled;
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.compiler.client.CompilerArguments;
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.compiler.client.EPCompilerProvider;
+import com.espertech.esper.runtime.client.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Configuration configuration = new Configuration();
+        configuration.getCommon().addEventType(KursAkcji.class);
+        EPRuntime epRuntime = EPRuntimeProvider.getDefaultRuntime(configuration);
+
+        EPDeployment deployment = compileAndDeploy(epRuntime,
+                "select istream a.data, a.kursOtwarcia - b.kursOtwarcia as roznica, a.spolka " +
+                        "from KursAkcji(a.spolka='Oracle').win:length(1) a, " +
+                        "KursAkcji(b.spolka='Oracle').win:length(2) b " +
+                        "where a.kursOtwarcia > b.kursOtwarcia");
+
+        ProstyListener prostyListener = new ProstyListener();
+
+        for (EPStatement statement : deployment.getStatements()) {
+            statement.addListener(prostyListener);
+        }
+
+        InputStream inputStream = new InputStream();
+        inputStream.generuj(epRuntime.getEventService());
+    }
+
+    public static EPDeployment compileAndDeploy(EPRuntime epRuntime, String epl) {
+        EPDeploymentService deploymentService = epRuntime.getDeploymentService();
+        CompilerArguments args = new CompilerArguments(epRuntime.getConfigurationDeepCopy());
+        EPDeployment deployment;
+        try {
+            EPCompiled epCompiled = EPCompilerProvider.getCompiler().compile(epl, args);
+            deployment = deploymentService.deploy(epCompiled);
+        } catch (EPCompileException e) {
+            throw new RuntimeException(e);
+        } catch (EPDeployException e) {
+            throw new RuntimeException(e);
+        }
+        return deployment;
+    }
+}
